5ee18bf9754a98ef386a6e78a441772162f1589b
정다희 <hacho_2011@naver.com>
2021-09-11 10:35:15 +0900

    feat:레이아웃 생성

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,8 +2,8 @@ import { Global, ThemeProvider } from "@emotion/react";
 import type { AppProps } from "next/app";
 import { FC } from "react";
 
-import { theme } from "@/styles/theme";
 import globalStyles from "@/styles/global";
+import { theme } from "@/styles/theme";
 
 const MyApp: FC<AppProps> = (props) => {
   const { Component, pageProps } = props;
