314032d452a7139f48a127d94536f6cb2ecc35e9
정다희 <hacho_2011@naver.com>
2021-10-18 16:53:52 +0900

    fix:색상 추가/AppLayout 수정

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,19 +1,23 @@
-import { DefaultTheme } from "@emotion/react";
+import { DefaultTheme } from '@emotion/react';
 
 const colors = {
-  brightGray: "#eee",
-  bg_sidebar: "#313A46",
-  bg_sidebar_over: "#262E38",
-  txt_disabled_gray: "#BCBFDA",
-  white: "#fff",
-  whiteGray: "#f5f5f5",
+  brightGray: '#eee',
+  bg_sidebar: '#313A46',
+  bg_sidebar_over: '#262E38',
+  txt_disabled_gray: '#BCBFDA',
+  white: '#fff',
+  whiteGray: '#f5f5f5',
+  primary: '#002F6C',
+  yellow: '#EFB661',
+  grey: '#f0f0f0',
+  darkGray: '4f4f4f',
 };
 
 const fontSizes = {
-  size11: "1.1rem",
-  size12: "1.2rem",
-  size15: "1.5rem",
-  size16: "1.6rem",
+  size11: '1.1rem',
+  size12: '1.2rem',
+  size15: '1.5rem',
+  size16: '1.6rem',
 };
 
 export const theme: DefaultTheme = {
